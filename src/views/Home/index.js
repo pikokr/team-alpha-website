a63d7b3754ad4578f72c8b869a59b1c15a3e4976
parangee <parangee9706@gmail.com>
2020-09-15 13:45:51 +0900

    fullpage.js에서 라이센스 키 넣으라고함

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,101 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 import styles from "./home.module.scss";
-import { motion } from "framer-motion";
 import { Button } from "discord-components-react";
-import RefreshButton from "../../components/RefreshButton";
 
-const scrollTo = (ref) =>
-  ref.current.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-
-function HomePage() {
-  const [guildData, setGuildData] = React.useState(null);
-
-  const refs = {
-    sec2: React.useRef(null),
+class HomePage extends Component {
+  ref = {
+    sec2: React.createRef(),
   };
 
-  fetch("https://discord.com/api/guilds/738294838063136808/embed.json")
-    .then((res) => res.json())
-    .then((res) => setGuildData(res));
-
-  return (
-    <div>
-      <div className={`${styles.background} ${styles.dark}`}>
-        <div>
-          <motion.h1
-            initial={{
-              scale: 0,
-              fontSize: 60,
-              margin: 0,
-            }}
-            animate={{
-              scale: 1,
-            }}
-          >
-            Team Alpha
-          </motion.h1>
-        </div>
-        <div style={{ marginTop: 20 }}>
-          <motion.div
-            initial={{
-              y: 25,
-              opacity: 0,
-            }}
-            animate={{
-              y: 0,
-              opacity: 1,
-              transition: {
-                delay: 0.5,
-              },
-            }}
-            transition={{
-              duration: 2,
-            }}
-            style={{ display: "inline-block" }}
-          >
-            <Button onClick={() => scrollTo(refs.sec2)}>더 알아보기</Button>
-          </motion.div>
-        </div>
-      </div>
-      <div className={styles.background} ref={refs.sec2}>
-        <div className={styles.center}>
-          <motion.h2
-            initial={{
-              scale: 0,
-              margin: 0,
-            }}
-            animate={{
-              scale: 1,
-            }}
-          >
-            Team Alpha 공식 서버{" "}
-            <RefreshButton
-              onClick={() =>
-                fetch(
-                  "https://discord.com/api/guilds/738294838063136808/embed.json"
-                )
-                  .then((res) => res.json())
-                  .then((res) => setGuildData(res))
-              }
-            />
-          </motion.h2>
-          {guildData ? (
-            <div>
-              <p>온라인 멤버 {guildData.members.length}명</p>
-              <Button onClick={() => window.open(guildData.instant_invite)}>
-                참가하기
-              </Button>
-            </div>
-          ) : (
-            "로딩중..."
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  render() {
+    return (
+      <ReactFullpage
+        scrollingSpeed={1000}
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className={`section ${styles.section} ${styles.dark}`}>
+                <h1 style={{
+                  fontSize: 60,
+                  margin: 0
+                }}>TEAM ALPHA</h1>
+                <p>디스코드 봇 제작 팀(?)</p>
+                <Button onClick={() => fullpageApi.moveSectionDown()}>
+                  더 알아보기
+                </Button>
+              </div>
+              <div className={`section ${styles.section}`}>
+                와 샌즈!
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    );
+  }
 }
 
 export default HomePage;
